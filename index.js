901ede261b4295938a44532b3da79602dcc44db1
leparan <leparanfelix16@gmail.com>
2025-03-22 10:56:13 +0300

    js events

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    // Define the clickAlert function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the event listener for the click event
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the addingEventListener function to activate the event listener
+  addingEventListener();
